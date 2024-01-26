f22e2a6b2ca9c4f38998a1185dff32889992b4d9
Kourosh <kurosh.mozafari@gmail.com>
2024-01-26 23:30:22 +0100

    using useQuery hook

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,12 +1,37 @@
 import React from "react";
-import { Layout } from "../components";
+import { Layout, QueryResult } from "../components";
+import { gql, useQuery } from '@apollo/client'
+import TrackCard from "../containers/track-card";
+import { Track } from "../__generated__/graphql";
 
+const TRACKS = gql`query GetTracks {
+  tracksForHome {
+    id
+    title
+    author {
+      id
+      name
+      photo
+    }
+    thumbnail
+    length
+    moduleCount
+  }
+
+}`
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+  return <Layout grid>
+    <QueryResult loading={loading} error={error} data={data}>
+      {
+        data?.tracksForHome.map((track: Track, index: number) => <TrackCard key={index} track={track} />)
+      }
+    </QueryResult>
+  </Layout>;
 };
 
 export default Tracks;
--- server/src/index.ts
+++ server/src/index.ts
@@ -13,11 +13,13 @@ const mocks = {
     Track: () => ({
         id: () => "track_01",
         title: () => "Astro Kitty, Space Explorer",
-        author: () => ({
-            name: "Grumpy Cat",
-            photo: "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
-        }),
-        thumbnai: () => "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+        author: () => {
+            return {
+                name: "Grumpy Cat",
+                photo: "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+            }
+        },
+        thumbnail: "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
         length: () => 1210,
         moduleCount: () => 6,
     }),
