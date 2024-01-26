30a3606bbbc07dd4abf7ae5411e78ac565c473ab
Kourosh <kurosh.mozafari@gmail.com>
2024-01-26 01:30:04 +0100

    Initial commit by me

---
+++ server/src/schema.ts
@@ -0,0 +1,34 @@
+import { gql } from 'graphql-tag';
+
+const typeDefs = gql`
+type Query {
+    "Query to get tracks array for the homepage grid"
+    tracksForHome: [Track!]!
+}
+
+"A track is a group of Modules that teaches about a specific topic"
+type Track {
+    id: ID!
+    "the track's title"
+    title: String!
+    "the track's main author"
+    author: Author!
+    "the track's main illustration to display in track card or track page detail"
+    thumbnail: String
+    "the track's approximate length to complete, in minutes"
+    length: Int
+    "the number of modues this track contains"
+    moduleCount: Int
+}
+
+"Author of a complete Track"
+type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture url"
+    photo: String
+}
+`;
+
+export default typeDefs;
